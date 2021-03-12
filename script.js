19eb686c616918372b2d32f57542a3cfc685a2cf
Ruslan <faleot.karas@gmail.com>
2021-03-12 16:44:47 +0300

    done

--- script.js
+++ script.js
@@ -14,9 +14,3 @@ function createDebounceFunction(cb, delayInMs) {
         timerHandler = setTimeout(cb, delayInMs);
     };
 }
-
-const log100 = () => console.log(100);
-const debounceLog100 = createDebounceFunction(log100, 5000);
-debounceLog100();
-setTimeout(debounceLog100, 2000);
-setTimeout(debounceLog100, 3000); 
